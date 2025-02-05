6902f9c368d0f1c8ea073a024fc1ccb2ea51a5e8
Willy-Thai <haruki.thai@gmail.com>
2025-02-05 20:07:06 +0100

    recuperation et affichage de la bdd depuis le front

--- components/Article.js
+++ components/Article.js
@@ -1,17 +1,93 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-// const Article = require('../models/articles');
+import styles from "../styles/Article.module.css";
+
+// import Card from 'react-bootstrap/Card';
+
+
+
+//Il y'aura une page article mais cett page article vient d'une autre page qui contient les articles
+//C'est quelle page qui contient les articles
+//On navigue depuis le Home
+//On peut soit cliquer direct sur la card d'un produit, soit naviguer vers une catégorie avant de cliquer sur le produit
+//Pour l'instant ca affiche tout les articles sur la page article
 
 function ArticlePage() {
+  const [allArticlesData, setAllArticlesData] = useState([]);
 
+// const auClick0 = () => {
+//   fetch(`http://localhost:3000/articles/articles`)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     if (data.result) {
+//       console.log(data)
+//       setAllArticlesData(data)
+//     }
+//   });
+// };
+
+
+useEffect(() => {
+  fetch(`http://localhost:3000/articles/articles`)
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.result) {
+      console.log(data)
+      setAllArticlesData(data.allArticles)
+    }
+  });
+}, [])
+
+console.log("le useState =", allArticlesData)
+
+
+
+const articles = allArticlesData.map((data, i) => {
+
+  const jeSaisPas0 = data.sizes.map((size, index) => (
+    <li key={index}>{size.size0}</li>
+  ))
+
+  const jeSaisPas1 = data.giSizes.map((sizeGi, index) => (
+    <li key={index}>{sizeGi.giSize0}</li>
+  ))
+
+  const jeSaisPas2 = data.colors.map((color, index) => (
+    <li key={index}>{color.colorName0}</li>
+  ))
+  
+  const jeSaisPas3 = data.photos.map((photo, index) => (
+    <li key={index}>{photo.photoUrl0}</li>
+  ))
+
+  
+  return (
+
+    <div key={i} className={styles.article}>
+
+      <p>{data.categorie}</p>
+      <p>{data.type}</p>
+      <p>{data.model}</p>
+      <p>{data.description}</p>
+      <p>{jeSaisPas0}</p>
+      <p>{jeSaisPas1}</p>
+      <p>{jeSaisPas2}</p>
+      <p>{jeSaisPas3}</p>
+      <p>{data.price}</p>
+      
+    </div>
+
+  )
+});
 
 
 
     return (
       <div>
-        <h4>The New-Yorker</h4>
-        <p>Description</p>
-        {/* <Button placeHolder="add to Cart"></Button> */}
+        {/* <button onClick={() => auClick0()}> je test </button> */}
+        <div className={styles.articlesContainer}>{articles}</div>
       </div>
     );
    }
